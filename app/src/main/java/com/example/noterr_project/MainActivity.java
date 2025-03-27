b5841d267f10ab1f62ed285e14083e0d69bc9979
abhi9 <abhinavvaidya2005@gmail.com>
2025-03-27 19:09:34 +0530

    changes made in main, notes main and notes editor

--- app/src/main/java/com/example/noterr_project/MainActivity.java
+++ app/src/main/java/com/example/noterr_project/MainActivity.java
@@ -1,24 +1,28 @@
 package com.example.noterr_project;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Find Button & Set Click Listener
+        Button buttonOpenNotes = findViewById(R.id.buttonOpenNotes);
+        buttonOpenNotes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open NotesMainActivity
+                Intent intent = new Intent(MainActivity.this, NotesMainActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
+
