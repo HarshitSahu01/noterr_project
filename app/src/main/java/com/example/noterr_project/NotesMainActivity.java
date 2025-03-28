ffbff45e5fb2803b137ded91406307a1b9051a21
abhi9 <abhinavvaidya2005@gmail.com>
2025-03-28 07:57:09 +0530

    changes made in main, notes main and notes editor

--- app/src/main/java/com/example/noterr_project/NotesMainActivity.java
+++ app/src/main/java/com/example/noterr_project/NotesMainActivity.java
@@ -2,30 +2,41 @@ package com.example.noterr_project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class NotesMainActivity extends AppCompatActivity {
-
-    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_main);
 
-        // Initialize Floating Action Button
-        fab = findViewById(R.id.fab);
-
-        // Opens NotesEditorActivity
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(NotesMainActivity.this, NotesEditorActivity.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                startActivity(new Intent(NotesMainActivity.this, NotesEditorActivity.class));
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_notes_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_private_notes) {
+            Intent intent = new Intent(this, LockPageActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
