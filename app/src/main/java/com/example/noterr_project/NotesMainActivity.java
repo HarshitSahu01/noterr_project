16c7ffaeb80001a05cd1001f21aa46411168cc9e
harshmundada25 <harshmundada97@gmail.com>
2025-03-30 16:20:21 +0530

    textview

--- app/src/main/java/com/example/noterr_project/NotesMainActivity.java
+++ app/src/main/java/com/example/noterr_project/NotesMainActivity.java
@@ -20,6 +20,8 @@ public class NotesMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_main);
+        Note[] notes = Note.getNotes();
+
 
         // Handle Floating Action Button click
         FloatingActionButton fab = findViewById(R.id.fab);
